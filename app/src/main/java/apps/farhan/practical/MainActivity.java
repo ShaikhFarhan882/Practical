14990e15b31c10ee72fc60ca1c04f86c3d64a4b0
Farhan Shaikh <blaxerf@gmail.com>
2025-11-02 12:39:16 +0530

    Initial Commit

---
+++ app/src/main/java/apps/farhan/practical/MainActivity.java
@@ -0,0 +1,27 @@
+package apps.farhan.practical;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import apps.farhan.practical.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.test.setOnClickListener(view -> {
+            //
+        });
+    }
+}
